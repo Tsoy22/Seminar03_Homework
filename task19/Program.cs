b1224898fae4f74016ea17e5ac945801480b1d86
Vitaly Tsoy <tsoyvitalik@icloud.com>
2022-09-15 00:07:19 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите число: ");
+string? digit = Convert.ToString(Console.ReadLine());
+if (digit[0] == digit[4] || digit[1] == digit[3])
+{
+  Console.WriteLine($"Число: {digit} - ДА ");
+}
+else Console.WriteLine($"Число: {digit} - НЕТ ");
+
